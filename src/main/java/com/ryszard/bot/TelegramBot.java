04ad10fe8fbfa7a42e0c12158c8a3c306fe714b0
rpoplavskij <rpoplavskij@gmail.com>
2020-02-11 08:39:21 +0300

    11.02 final commit

--- src/main/java/com/ryszard/bot/TelegramBot.java
+++ src/main/java/com/ryszard/bot/TelegramBot.java
@@ -1,5 +1,6 @@
 package com.ryszard.bot;
 
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -7,6 +8,17 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import static com.ryszard.bot.Connect.getDataFromDb;
 
 public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "gregarious_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "954074327:AAFPFs-IaZ8tQNTP-Vo5Ylyczsr5E4Grv34";
+    }
+
     @Override
     public void onUpdateReceived(Update update){
 
@@ -18,30 +30,32 @@ public class TelegramBot extends TelegramLongPollingBot {
             message.setChatId(update.getMessage().getChatId());
             try {
                 execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            } catch(TelegramApiException e){
+                sendInfo(update, "Города с таким названием нет в нашей базе.");
             }
-
         }
         String dataFromDb = getDataFromDb(command);
-
         message.setText(dataFromDb);
         message.setChatId(update.getMessage().getChatId());
         try {
             execute(message);
-        } catch (TelegramApiException e) {
+        }  catch (TelegramApiException e) {
             e.printStackTrace();
         }
 
     }
 
-    @Override
-    public String getBotUsername() {
-        return "gregarious_bot";
+    private void sendInfo(Update update, String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText(message);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public String getBotToken() {
-        return "954074327:AAFPFs-IaZ8tQNTP-Vo5Ylyczsr5E4Grv34";
-    }
+
+
 }
